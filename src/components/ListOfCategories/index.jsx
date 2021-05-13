52c51c2b49e35ae0a5e9f8cf1e61c78df7dd8ca6
chrockny <chrockny1007@gmail.com>
2021-05-12 20:14:47 -0500

    Custom hook

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,15 +1,46 @@
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import {Category} from '../Category/index';
 import {List, Item} from './styles';
-import {categories as mockCategories} from './db.json';
+
+function useCategoriesData(){
+    const [categories,setCategories] = useState([]);
+    const [showFixed,setShowFixed] = useState(false);
+    useEffect(function(){
+        window.fetch('https://petgram-chrockny.vercel.app/categories')
+        .then(res => res.json())
+        .then(response =>{
+            setCategories(response)
+        })
+        .catch(e=>console.log(e))
+    },[])
+    return {categories};
+}
 
 export const ListOfCategories = () =>{
-    const [categories,setCategories] = useState(mockCategories);
+    const {categories}= useCategoriesData();
+    const [showFixed,setShowFixed] = useState(false);
+
+    useEffect(function(){
+        const onScroll = e=>{
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed);
+        }
+        document.addEventListener('scroll',onScroll);
+
+        return()=>document.removeEventListener('scroll',onScroll);
+    })
+
+    const renderList = (fixed) =>(
+    <List fixed={fixed}>
+        {
+        categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
+        }
+    </List>   
+    )
     return(
-        <List>
-            {
-                categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
-            }
-        </List>
+        <>
+        {renderList()}
+        {showFixed && renderList(true)}
+        </>
     )
 }
--- src/components/ListOfCategories/styles.jsx
+++ src/components/ListOfCategories/styles.jsx
@@ -1,4 +1,29 @@
-import styled from 'styled-components';
+import styled ,{keyframes,css}from 'styled-components';
+const bounceDownKeyFrames = keyframes`
+ 0% {
+    top: -70px;
+  }
+
+  25% {
+    top: 0px;
+  }
+
+  40%{
+    top: 10px
+  }
+
+  65%{
+    top: -3px
+  }
+
+  100% {
+    top: 0px;
+  }
+`
+export const bounceDown = ({ time = '1s', type = 'ease' } = {}) =>
+  css`
+    animation: ${time} ${bounceDownKeyFrames} ${type};
+  `
 
 export const List = styled.ul`
 display:flex;
@@ -7,6 +32,21 @@ width:100%;
 ::-webkit-scrollbar {
     -webkit-appearance: none;
 }
+${props => props.fixed && css`
+${bounceDown()};
+    background:#fff;
+    border-radius:60px;
+    box-shadow:0 0 20px rgba(0,0,0,.3);
+    left:0;
+    margin: 0 auto;
+    max-width:400px;
+    padding:5px;
+    position:fixed;
+    right:0;
+    top:-20px;
+    transform:scale(.5);
+    z-index:1;
+`}
 `
 export const Item =  styled.li`
 padding:0 8px;
